9355c9a53ba467fbd3c0da9dc4f202df064ca278
Joshua Pease <joshua.pease@viget.com>
2024-05-16 08:17:46 -0700

    [#2] Tailwind and PostCSS (#8)
    * [#2] Install Tailwind / PostCSS
    
    * [#2] Add `postcss-pxtorem`
    
    * [#2] Add `cssnano` for production builds
    
    * [#2] Feedback: Wrap all css in `@layer base`

---
+++ postcss.config.js
@@ -0,0 +1,37 @@
+export default {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+        'postcss-pxtorem': {
+            rootValue: 16,
+            unitPrecision: 5,
+            propList: [
+                'font',
+                'font-size',
+                'line-height',
+                'letter-spacing',
+                'border*',
+                'background*',
+                'grid*',
+                'top',
+                'left',
+                'bottom',
+                'right',
+                'inset',
+                'width',
+                'height',
+                'margin*',
+                'padding*',
+                'max-*',
+                'min-*',
+                'gap*',
+            ],
+            selectorBlackList: [],
+            replace: true,
+            mediaQuery: true,
+            minPixelValue: 0,
+            exclude: /node_modules/i,
+        },
+        ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
+    }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './templates/**/*.twig',
+    './src/**/*.{js,jsx,ts,tsx,svg}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
