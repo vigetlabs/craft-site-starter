85ffb315f5dea7199dd9185710994db76572a8a1
Joshua Pease <joshua.pease@viget.com>
2024-05-20 16:39:11 -0700

    [#3] Install PHP ECS and run fix

--- install-scripts/ScriptHelpers.php
+++ install-scripts/ScriptHelpers.php
@@ -5,44 +5,44 @@ class ScriptHelpers
     /**
      * Escape character
      */
-    const ESC = "\033";
+    public const ESC = "\033";
 
     /**
      * ANSI colours
      */
-    const ANSI_BLACK = self::ESC . "[30m";
-    const ANSI_RED = self::ESC . "[31m";
-    const ANSI_GREEN = self::ESC . "[32m";
-    const ANSI_YELLOW = self::ESC . "[33m";
-    const ANSI_BLUE = self::ESC . "[34m";
-    const ANSI_MAGENTA = self::ESC . "[35m";
-    const ANSI_CYAN = self::ESC . "[36m";
-    const ANSI_WHITE = self::ESC . "[37m";
+    public const ANSI_BLACK = self::ESC . "[30m";
+    public const ANSI_RED = self::ESC . "[31m";
+    public const ANSI_GREEN = self::ESC . "[32m";
+    public const ANSI_YELLOW = self::ESC . "[33m";
+    public const ANSI_BLUE = self::ESC . "[34m";
+    public const ANSI_MAGENTA = self::ESC . "[35m";
+    public const ANSI_CYAN = self::ESC . "[36m";
+    public const ANSI_WHITE = self::ESC . "[37m";
 
     /**
      * ANSI background colours
      */
-    const ANSI_BACKGROUND_BLACK = self::ESC . "[40m";
-    const ANSI_BACKGROUND_RED = self::ESC . "[41m";
-    const ANSI_BACKGROUND_GREEN = self::ESC . "[42m";
-    const ANSI_BACKGROUND_YELLOW = self::ESC . "[43m";
-    const ANSI_BACKGROUND_BLUE = self::ESC . "[44m";
-    const ANSI_BACKGROUND_MAGENTA = self::ESC . "[45m";
-    const ANSI_BACKGROUND_CYAN = self::ESC . "[46m";
-    const ANSI_BACKGROUND_WHITE = self::ESC . "[47m";
+    public const ANSI_BACKGROUND_BLACK = self::ESC . "[40m";
+    public const ANSI_BACKGROUND_RED = self::ESC . "[41m";
+    public const ANSI_BACKGROUND_GREEN = self::ESC . "[42m";
+    public const ANSI_BACKGROUND_YELLOW = self::ESC . "[43m";
+    public const ANSI_BACKGROUND_BLUE = self::ESC . "[44m";
+    public const ANSI_BACKGROUND_MAGENTA = self::ESC . "[45m";
+    public const ANSI_BACKGROUND_CYAN = self::ESC . "[46m";
+    public const ANSI_BACKGROUND_WHITE = self::ESC . "[47m";
 
     /**
      * ANSI styles
      */
-    const ANSI_BOLD = self::ESC . "[1m";
-    const ANSI_ITALIC = self::ESC . "[3m"; // limited support. ymmv.
-    const ANSI_UNDERLINE = self::ESC . "[4m";
-    const ANSI_STRIKETHROUGH = self::ESC . "[9m";
+    public const ANSI_BOLD = self::ESC . "[1m";
+    public const ANSI_ITALIC = self::ESC . "[3m"; // limited support. ymmv.
+    public const ANSI_UNDERLINE = self::ESC . "[4m";
+    public const ANSI_STRIKETHROUGH = self::ESC . "[9m";
 
     /**
      * Clear all ANSI styling
      */
-    const ANSI_CLOSE = self::ESC . "[0m";
+    public const ANSI_CLOSE = self::ESC . "[0m";
 
     public static function output(string $message): int|false
     {
---
+++ modules/todo.php
@@ -0,0 +1,3 @@
+<?php
+
+// TODO add a default module
