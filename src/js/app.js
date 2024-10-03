a093a7fc0e3cb69720db02e6f8b68813fa79c91a
Joshua Pease <joshua.pease@viget.com>
2024-10-03 14:02:08 -0700

    [#51] Basic desktop nav.
    The desktop and mobile nav markup will be their own partials.

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,12 @@ import '../css/app.css'
 
 import Alpine from 'alpinejs'
 import focus from '@alpinejs/focus'
+import nav from './alpine/nav'
 
 Alpine.plugin(focus)
 
 // @ts-ignore
 window.Alpine = Alpine
 
+Alpine.data('nav', nav)
 Alpine.start()
