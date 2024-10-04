a68bbd8fec1af62e5ee5ebbc66b997a78852f549
Joshua Pease <joshua.pease@viget.com>
2024-10-04 15:55:42 -0700

    [#51] Adds mobile nav

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,11 @@ import '../css/app.css'
 
 import Alpine from 'alpinejs'
 import focus from '@alpinejs/focus'
+import collapse from '@alpinejs/collapse'
 import nav from './alpine/nav'
 
 Alpine.plugin(focus)
+Alpine.plugin(collapse)
 
 // @ts-ignore
 window.Alpine = Alpine
