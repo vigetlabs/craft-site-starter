29781ee39fbfacce27ca545cfb791d3a73c85fed
Joshua Pease <joshua.pease@viget.com>
2024-12-18 09:34:53 -0800

    [#52] Allow for custom id, class, and use html5 validation

--- config/tailwind/forms.js
+++ config/tailwind/forms.js
@@ -57,7 +57,7 @@ export default plugin(({ addComponents }) => {
         backgroundRepeat: 'no-repeat',
         '@apply appearance-none pr-32': {},
 
-        '[data-theme="dark"] &': {
+        '.dark &': {
           backgroundImage: `url("data:image/svg+xml, %3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath fill='%23ffffff' d='M12 5.83 15.17 9l1.41-1.41L12 3 7.41 7.59 8.83 9 12 5.83zm0 12.34L8.83 15l-1.41 1.41L12 21l4.59-4.59L15.17 15 12 18.17z'/%3E%3C/svg%3E")`,
         },
         '& option': {
