103a7e1b4d21ca63e30e609143805bafa478ef81
Joshua Pease <joshua.pease@viget.com>
2025-01-29 15:28:17 -0800

    Fix typos lint-staged.config.js

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -9,7 +9,7 @@ export default {
   ],
   '*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --ignore-unknown --write'],
   // Negate the JS files above so there isn't a race condition
-  // This runs prettier on every tyep of fiel but ths JS extensions listed below
+  // This runs prettier on every type of file other than the extensions listed below
   // https://github.com/lint-staged/lint-staged?tab=readme-ov-file#task-concurrency
   '!(*.js|*.jsx|*.ts|*.tsx)': 'prettier --ignore-unknown --write',
 }
