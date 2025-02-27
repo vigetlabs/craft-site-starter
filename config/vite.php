90390612ebefde46220699828e9b458a8e94b9be
Joshua Pease <joshua.pease@viget.com>
2025-02-27 15:00:05 -0800

    [#80] Simplify vite.php
    The config can be way simpler if you only allow access from https

--- config/vite.php
+++ config/vite.php
@@ -2,21 +2,10 @@
 
 use craft\helpers\App;
 
-
-// Decides which port to use for devServerPublic. This allows
-// you to visit the http or https port. If you plant to only
-// use http, make sure that PRIMARY_SITE_URL is set to http
-//
-// Match ports to .ddev/config.yaml -> web_extra_exposed_ports
-$host = Craft::$app->getRequest()->getIsConsoleRequest()
-    ? App::env('PRIMARY_SITE_URL')
-    : Craft::$app->getRequest()->getHostInfo();
-$httpPort = 3000;
-$httpsPort = 3001;
-$devServerPort = str_starts_with($host, 'https') ? $httpsPort : $httpPort;
-
 return [
-    'devServerPublic' => "$host:$devServerPort",
+    // Match to https port in .ddev/config.yaml -> web_extra_exposed_ports
+    // Only works when using HTTPS on DDEV.
+    'devServerPublic' => App::env('PRIMARY_SITE_URL') . ':3001',
     'serverPublic' => '/dist/',
     'useDevServer' => App::env('CRAFT_ENVIRONMENT') === 'dev',
     'manifestPath' => '@webroot/dist/.vite/manifest.json',
