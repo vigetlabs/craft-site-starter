2c1a08bba5d8ae6de39a701d4518f470c8aebbd1
Joshua Pease <joshua.pease@viget.com>
2025-03-13 15:49:24 -0700

    [#93] Image component cleanup & documentation

---
+++ config/imager-x-transforms.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * USING SERVERLESS IMAGE TRANSFORMS?
+ * @see https://github.com/spacecatninja/craft-imager-x-aws-serverless-transformer
+ *
+ * If you are using Serverless Image Transforms (which are a Site Starter Default),
+ * automatic genration of transforms is not necessary and you may delete this file.
+ *
+ *
+ * USING AUTOMATIC TRANSFORM GENRATION?
+ * @see https://imager-x.spacecat.ninja/usage/generate.html
+ * @see https://imager-x.spacecat.ninja/usage/named-transforms.html
+ *
+ * Generating transforms at runtime is a resource heavy operation.
+ *
+ * Named transforms give Imager X the information needed to pre-generate
+ * transforms with a queue job immediately after uploading a new asset.
+ */
+return [
+    'exampleNamedTransform' => [
+        'displayName' => 'Example Named Transform',
+        'transforms' => [
+            ['width' => 300],
+            ['width' => 700],
+        ],
+        'defaults' => [
+            'ratio' => 9 / 16,
+            'format' => 'webp',
+        ],
+    ],
+];
