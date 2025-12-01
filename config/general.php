381be658da321f0e132696f865287b127fb24eb5
Max Fenton <max.fenton@viget.com>
2025-12-01 11:16:31 -0500

    [n/a] Update general config

--- config/general.php
+++ config/general.php
@@ -10,18 +10,31 @@
 
 use craft\config\GeneralConfig;
 use craft\helpers\App;
+use craft\helpers\StringHelper;
+
+$environment = App::env("CRAFT_ENVIRONMENT");
+$isDev = $environment === "dev";
+$isProduction = $environment === "production";
 
 return GeneralConfig::create()
-    // Set the default week start day for date pickers (0 = Sunday, 1 = Monday, etc.)
-    ->defaultWeekStartDay(1)
-    // Prevent generated URLs from including "index.php"
+    ->allowAdminChanges($isDev)
+    ->allowUpdates($isDev)
+    ->backupOnUpdate(false)
+    ->convertFilenamesToAscii(true)
+    ->defaultWeekStartDay(1) // Monday
+    ->devMode($isDev)
+    ->disallowRobots(!$isProduction)
+    ->enableGql(false)
+    ->enableTemplateCaching(!$isDev)
+    ->limitAutoSlugsToAscii(true)
     ->omitScriptNameInUrls()
-    // Preload Single entries as Twig variables
     ->preloadSingles()
-    // Prevent user enumeration attacks
     ->preventUserEnumeration()
-    // Set the @webroot alias so the clear-caches command knows where to find CP resources
+    ->timezone('America/New_York')
+    ->useEmailAsUsername()
+    ->verificationCodeDuration("P1W")
     ->aliases([
         '@webroot' => dirname(__DIR__) . '/web',
-    ])
-;
+        '@web' => App::env('PRIMARY_SITE_URL'),
+        '@primarySiteUrl' => StringHelper::removeRight(App::env('PRIMARY_SITE_URL'), '/'),
+    ]);
