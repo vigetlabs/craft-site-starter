af340901e5f91d01c87627b05864233057e26c7f
Max Fenton <max.fenton@viget.com>
2025-12-01 11:24:13 -0500

    Add default config for Embedded Assets

---
+++ config/embeddedassets.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Embedded Assets Configuration
+ *
+ * @link https://github.com/spicywebau/craft-embedded-assets/blob/5.x/docs/configuration.md
+ */
+return [
+    'preventNonWhitelistedUploads' => true,
+    'cacheDuration' => '21600', // 6 hours
+];
